5b8e01e2054c0e1bdeb2fa51334af53a2429fb3b
heshan <heshansandakelum2018@gmail.com>
2026-05-19 18:48:06 +0530

    initialize project with Dependencies

---
+++ src/main/java/com/checkmate_portal/chessclub/ChessclubApplication.java
@@ -0,0 +1,13 @@
+package com.checkmate_portal.chessclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkmate_portal/chessclub/ChessclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkmate_portal.chessclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
