c628ef152e82d18ef4e5cd50b249fce0386f142c
heshan <heshansandakelum2018@gmail.com>
2026-05-26 09:06:45 +0530

    complete service layer

--- src/main/java/com/checkmate_portal/chessclub/service/PlayerService.java
+++ src/main/java/com/checkmate_portal/chessclub/service/PlayerService.java
@@ -5,6 +5,7 @@ import com.checkmate_portal.chessclub.repository.PlayerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerService {
@@ -20,4 +21,25 @@ public class PlayerService {
     public List<Player> getAllPlayers(){
         return playerRepository.findAll();
     }
+    public Optional<Player> getPlayerById(Long id){
+        return playerRepository.findById(id);
+    }
+
+    public Player updatePlayer(Long id,Player updatePlayer){
+        Player player=playerRepository.findById(id).orElseThrow(()->new RuntimeException("Player not found"));
+
+        player.setName(updatePlayer.getName());
+        player.setCountry(updatePlayer.getCountry());
+        player.setAge(updatePlayer.getAge());
+        player.setFideRating(updatePlayer.getFideRating());
+        player.setExperienceYears(updatePlayer.getExperienceYears());
+        player.setLevel(updatePlayer.getLevel());
+
+        return playerRepository.save(player);
+    }
+    public void deletePlayer(Long id){
+        playerRepository.findById(id).orElseThrow(()->new RuntimeException("Player not found"));
+        playerRepository.deleteById(id);
+    }
+
 }
