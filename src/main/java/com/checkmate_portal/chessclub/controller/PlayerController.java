e3c7b4747381aee95ccf428ee3da4eaa5b034706
heshan <heshansandakelum2018@gmail.com>
2026-05-26 09:30:02 +0530

    complete controller layer

--- src/main/java/com/checkmate_portal/chessclub/controller/PlayerController.java
+++ src/main/java/com/checkmate_portal/chessclub/controller/PlayerController.java
@@ -1,8 +1,12 @@
 package com.checkmate_portal.chessclub.controller;
 
+import com.checkmate_portal.chessclub.entity.Player;
 import com.checkmate_portal.chessclub.service.PlayerService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping
@@ -13,6 +17,32 @@ public class PlayerController {
         this.playerService=playerService;
     }
 
-    
+    @PostMapping
+    public Player createPlayer(@RequestBody Player player){
+        return playerService.createPlayer(player);
+    }
+
+    @GetMapping
+    public List<Player> getAllPlayer(){
+        return playerService.getAllPlayers();
+    }
+
+    @GetMapping
+    public Optional<Player> getPlayerById(@PathVariable Long id){
+        return playerService.getPlayerById(id);
+    }
+
+    @PutMapping
+    public Player update(@PathVariable Long id,@RequestBody Player updatePlayer){
+        return playerService.updatePlayer(id,updatePlayer);
+    }
+
+    public String deletePlayer(@PathVariable Long id){
+        playerService.deletePlayer(id);
+        return "Player delete successfully";
+    }
+
+
+
 
 }
