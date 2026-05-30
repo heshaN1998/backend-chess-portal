5c3a148df366ce717dcf814cfa39d126ffe71dfb
heshan <heshansandakelum2018@gmail.com>
2026-05-30 23:55:45 +0530

    complete validation error part in exception handler

--- src/main/java/com/checkmate_portal/chessclub/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/checkmate_portal/chessclub/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,29 @@
 package com.checkmate_portal.chessclub.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.sun.tools.javac.jvm.ByteCodes.error;
+
+@RestController
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> handleValidation(MethodArgumentNotValidException ex){
+         Map<String,String> errors=new HashMap<>();
+         ex.getBindingResult().getFieldErrors().forEach(error->errors.put(error.getField(),error.getDefaultMessage()));
+
+         Map<String,Object> response = new HashMap<>();
+         response.put("message","validation failed");
+         response.put("error",errors);
+
+         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 }
