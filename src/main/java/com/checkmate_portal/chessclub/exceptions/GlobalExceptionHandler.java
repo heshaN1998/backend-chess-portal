d8eaf7545538d57506fe6b5df292fd30dc9a75f0
heshan <heshansandakelum2018@gmail.com>
2026-05-30 23:38:37 +0530

    starting exception secttion

---
+++ src/main/java/com/checkmate_portal/chessclub/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,5 @@
+package com.checkmate_portal.chessclub.exceptions;
+
+public class GlobalExceptionHandler {
+
+}
---
+++ src/main/java/com/checkmate_portal/chessclub/exceptions/ResourceNotFound.java
@@ -0,0 +1,7 @@
+package com.checkmate_portal.chessclub.exceptions;
+
+public class ResourceNotFound extends RuntimeException{
+    public ResourceNotFound(String message){
+        super(message);
+    }
+}
