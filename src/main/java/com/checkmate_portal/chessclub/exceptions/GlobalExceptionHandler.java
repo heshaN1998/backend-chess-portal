0475bde45c47d61ca4965ba60bef5cc90e13bf3e
heshan <heshansandakelum2018@gmail.com>
2026-05-31 23:15:38 +0530

    completed exception handling

--- src/main/java/com/checkmate_portal/chessclub/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/checkmate_portal/chessclub/exceptions/GlobalExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import static com.sun.tools.javac.jvm.ByteCodes.error;
 
 @RestController
@@ -25,5 +24,19 @@ public class GlobalExceptionHandler {
          response.put("error",errors);
 
          return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFound.class)
+    public ResponseEntity<Map<String, Object>> handleNotFound(ResourceNotFound ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGeneral(Exception ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Something went wrong");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
